a93479b67620e3240bfc07ad767bb7751b7fb6a7
tomtiao <tomtiao1234@gmail.com>
2020-10-09 06:51:47 +0800

    部分完成登录注册流程

--- login/login.js
+++ login/login.js
@@ -1,9 +1,30 @@
-function submitCheck() {
-    document.querySelector('.submit_btn').addEventListener('click', e => {
-        
+function submitHandler() {
+    const form_self = document.querySelector('.form');
+    const submit_btn = document.querySelector('.submit_btn');
+    function submitCheck() {
+
+    }
+    function submit() {
+        submitCheck();
+        const requestURL = '/user/login';
+        let form_data = new FormData(form_self);
+
+        return fetch(requestURL, {
+            method: 'POST',
+            body: form_data
+        })
+            .then(res => res.json())
+    }
+    form_self.addEventListener('click', e => {
+        if (e.target === submit_btn) {
+            e.preventDefault();
+            submit()
+                .then(console.log)
+                .catch(console.log);
+        }
     });
 }
 
 window.addEventListener('load', () => {
-    
+    submitHandler();
 });
--- register/register.js
+++ register/register.js
@@ -1,14 +1,41 @@
-function submit() {
-    const form_obj = document.querySelector('.form');
+function submitHandler() {
+    const form_self = document.querySelector('.form');
     const submit_btn = document.querySelector('.submit_btn');
-    form_obj.addEventListener('click', e => {
+    function submitCheck() { }
+    function submit() {
+        submitCheck();
+        const requestURL = '/user/register';
+        let form_data = new FormData(form_self);
+        return fetch(requestURL, {
+            method: 'POST',
+            body: form_data
+        })
+            .then(res => res.json());
+    };
+    form_self.addEventListener('click', e => {
         if (e.target === submit_btn) {
             e.preventDefault();
-            console.log(e.target);
+            submit()
+                .then(console.log)
+                .catch(console.log);
         }
-    })
+    });
+}
+
+function verificationCodeHandler() {
+    function getVerificationCode(target) {
+        const requestURL = '/checkCode' + '?' + new Date().getTime();
+        target.src = requestURL;
+    }
+    const verificationCodeImg = document.querySelector('.securityCode_img');
+
+    verificationCodeImg.addEventListener('click', e => {
+        getVerificationCode(verificationCodeImg);
+    });
+    getVerificationCode(verificationCodeImg);
 }
 
 window.addEventListener('load', () => {
-    // submit();
+    submitHandler();
+    verificationCodeHandler();
 });
