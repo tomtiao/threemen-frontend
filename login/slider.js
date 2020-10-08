8d9ea8ea7936106a3bed090cc5b2a6e5bf2ca4c5
tomtiao <tomtiao1234@gmail.com>
2020-10-09 06:45:02 +0800

    修复轮播图不立即加载的问题

--- login/slider.js
+++ login/slider.js
@@ -3,6 +3,9 @@ function slider() {
     const IMG_LIST_LEN = 3;
     let index = 0;
     let img_name;
+    img_name = index % IMG_LIST_LEN;
+    SLIDE.style.backgroundImage = `url(\"/static/img/login/${img_name}.jpg\")`;
+    index++;
     setInterval(() => {
         img_name = index % IMG_LIST_LEN;
         SLIDE.style.backgroundImage = `url(\"/static/img/login/${img_name}.jpg\")`;
