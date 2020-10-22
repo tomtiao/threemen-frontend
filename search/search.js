03132c90ad4450192d17e4b581008597daaa7d1e
tomtiao <47553271+tomtiao@users.noreply.github.com>
2020-10-22 08:50:11 +0800

    dunno

--- search/search.js
+++ search/search.js
@@ -1,3 +1,4 @@
+"use strict";
 function filterHandler() {
     let lists = document.querySelectorAll('.list');
     let btns = document.querySelectorAll('.list_btn');
@@ -53,44 +54,177 @@ function mobi() {
     });
 }
 
-function makeRequest() {
-    function requestQuest() {
+
+function displayResult() {
+
+    const list = document.querySelector('.select_page_list');
+    function initPageList(totalPage) {
+        totalPage = totalPage || 1;
+
+        const next_page_btn_item = document.querySelector('.select_page_list .list_item.next_page');
+        let create_page_btn = (page) => {
+            let new_list_item = document.createElement('li');
+            new_list_item.classList.add('list_item');
+
+            let new_page_btn = document.createElement('button');
+            new_page_btn.classList.add('list_btn');
+            new_page_btn.dataset.page = page;
+            new_page_btn.textContent = page;
+            new_list_item.append(new_page_btn);
+
+            list.insertBefore(new_list_item, next_page_btn_item);
+        };
+
+        for (let i = 1; i <= totalPage; i++) {
+            create_page_btn(i);
+        }
+    }
+
+    function changePage(info_array) {
+        const info_list = document.querySelector('.info_list');
+        // expected info array
+        function updateList(info_o) {
+            let new_info_item = document.createElement('li');
+            new_info_item.classList.add('list_item');
+
+            let new_info_title = document.createElement('h1');
+            new_info_title.classList.add('item_title');
+            new_info_title.textContent = info_o['position'];
+
+            let new_second_list = document.createElement('ul');
+            new_second_list.classList.add('item_brief');
+
+            let new_second_list_item = document.createElement('li');
+            new_second_list_item.classList.add('brief_item');
+            new_second_list_item.textContent = info_o['fixSeat'];
+
+            new_second_list.append(new_second_list_item);
+
+            new_second_list_item.textContent = info_o['temSeat'];
+            new_second_list.append(new_second_list_item);
+
+            new_info_item.append(new_info_title, new_second_list);
+
+            info_list.append(new_info_item);
+        }
+
+        function clearList() {
+            while (info_list.firstChild) {
+                info_list.removeChild(info_list.firstChild);
+            }
+        }
+
+        clearList();
+        for (let info_o of info_array) {
+            updateList(info_o);
+        }
+    }
+
+    let currentPage = 1;
+    let totalPage;
+    function clickBtnHandler(info_array) {
+        function listenPageList() {
+            // param expects li
+            let activeBtn = (list_item) => {
+                if (!list_item.classList.contains('prev_page')
+                    && !list_item.classList.contains('next_page')) {
+                    for (let ele of list.children) {
+                        if (ele.tagName === 'LI' && ele.firstElementChild.classList.contains('active')) {
+                            ele.firstElementChild.classList.remove('active');
+                        }
+                    }
+                    list_item.firstElementChild.classList.add('active');
+                }
+            };
+
+            list.addEventListener('click', e => {
+                if (e.target.tagName === 'BUTTON') {
+                    switch (e.target.parentNode.className) {
+                        case 'list_item prev_page':
+                            if (currentPage !== 1) {
+                                currentPage--;
+                                console.log(currentPage);
+                                changePage(info_array);
+                            } else {
+                                console.log('已经是首页了！');
+                            }
+                            break;
+                        case 'list_item next_page':
+                            if (currentPage != totalPage) {
+                                currentPage++;
+                                console.log(currentPage);
+                                changePage(info_array);
+                            } else {
+                                console.log('已经是尾页了！');
+                            }
+                            break;
+                        default:
+                            currentPage = e.target.dataset.page;
+                            console.log(currentPage);
+                            activeBtn(e.target.parentNode);
+                            changePage(info_array);
+                            break;
+                    }
+                }
+            });
+        }
+
+        listenPageList();
+    }
+
+
+    function makeRequest() {
         const requestURL = '/workStudyServlet/showWorkStudy';
         let request = {
-            currentPage: Math.random() * 10,
-            pageSize: 5
+            currentPage: currentPage,
+            pageSize: 5,
+            content: ""
         };
+
+        let form_data = new FormData();
+        let key = Object.keys(request);
+        key.forEach((key) => {
+            form_data.append(key, request[key]);
+        })
 
         return fetch(requestURL, {
             method: 'POST',
-            body: JSON.stringify(request)
+            body: form_data,
+            credentials: "same-origin"
         })
-            .then(res => res.json());
+            .then(res => res.json()).catch(console.log);
     }
-    return requestQuest()
-        .catch(console.log);
+
+    let data = makeRequest();
+    data.then(data_obj => {
+        let obj = data_obj;
+        // initPageList(obj['totalPage']);
+        totalPage = obj['totalPage'];
+        initPageList(10);
+        currentPage = obj['currentPage'];
+        // 设置第一页按钮样式
+        document.querySelector('.select_page_list .list_item:not(.prev_page) .list_btn')
+            .classList.add('active');
+        clickBtnHandler(obj['WorkStudyInfo']);
+    });
+
+
 }
 
 function displayResult() {
-    // let response = makeRequest();
-    let response = {
-        currentPage: Math.random() * 10,
-        pageSize: 5,
-        totalPage: 10,
-        totalCount: 50,
-        WorkStudyInfo: {
-            // 'location': location,
-            fixSeat: Math.random() * 10,
-            temSeat: Math.random() * 10
-        }
-    };
-    
+    makeRequest();
+    totalPage;
+    initPageList();
+    currentPage;
+    listenPageList();
+    changePage();
+    updateList();
 }
 
 window.addEventListener('load', () => {
-    filterHandler();
+    // filterHandler();
     toTop();
     goBack();
-    mobi();
-    makeRequest();
+    // mobi();
+    displayResult();
 });
