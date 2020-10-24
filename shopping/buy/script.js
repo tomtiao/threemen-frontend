27fcac233b79f5581cfa94ab3bbf0a3699e956bd
tomtiao <47553271+tomtiao@users.noreply.github.com>
2020-10-24 10:03:25 +0800

    move js that used multiple time to static folder

--- shopping/buy/script.js
+++ shopping/buy/script.js
@@ -35,32 +35,9 @@ function suggestionHandler() {
     });
 }
 
-function autoCompleteUserInfo() {
-    function fetchUserInfo() {
-        const requestURL = '/order/preShowOrder';
 
-        return fetch(requestURL, {
-            method: "POST",
-            credentials: "same-origin"
-        }).then(res => res.json()).catch(console.log);
-    }
-
-    function setUserInfo({ [0]: user, [1]: userInfo }) {
-        let name_input = document.getElementById('name');
-        let phone_input = document.getElementById('phone');
-        let address_input = document.getElementById('address');
-
-        name_input.value = user['nickname'];
-        phone_input.value = userInfo['phone'];
-        address_input.value = userInfo['address'];
-    }
-
-    let user_data = fetchUserInfo();
-    user_data.then(setUserInfo);
-}
 
 window.addEventListener('load', () => {
     siteCustomHandler();
     suggestionHandler();
-    autoCompleteUserInfo();
 });
