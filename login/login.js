eb62f8d068a9e43f631fa6b779a647ccfde4fc59
tomtiao <tomtiao1234@gmail.com>
2020-11-16 22:57:05 +0800

    add encryption to login

--- login/login.js
+++ login/login.js
@@ -1,46 +1,85 @@
 "use strict";
-
 function submitHandler() {
     const form_self = document.querySelector('.form');
     const submit_btn = document.querySelector('.submit_btn');
+    if (!form_self || !submit_btn) {
+        throw Error(`could not found form_self or submit_btn`);
+    }
     function submitCheck() {
-        if (document.getElementById('account').value != ''
-            && document.getElementById('password').value != '') {
+        const username = document.getElementById('account');
+        const password = document.getElementById('password');
+        if (!username || !password) {
+            throw Error(`could not found username or password`);
+        }
+        if (username.value != ''
+            && password.value != '') {
             return true;
-        } else {
+        }
+        else {
             alert('账号和密码不能为空！');
             return false;
         }
     }
-
-    function submit() {
+    function getNonceAndPubkey() {
+        const requestURL = '/saveServlet/getNonceAndPublicKey';
+    
+        return fetch(requestURL, {
+            credentials: 'same-origin'
+        }).then(res => res.json()).catch(console.log);
+    }
+    function encryptString(public_key, str) {
+        const encrypt = new JSEncrypt();
+    
+        encrypt.setPublicKey(public_key);
+    
+        return encrypt.encrypt(str);
+    }
+    
+    function makeRequest(username, password, nonce, pubkey) {
         const requestURL = '/user/login';
-        let form_data = new FormData(form_self);
-        let url_params = new URLSearchParams();
-
-        for (let pair of form_data) {
-            url_params.append(pair[0], pair[1]);
-        }
-
+    
+        const urlParams = new URLSearchParams();
+    
+        urlParams.append('account', username);
+        urlParams.append('password', password);
+        urlParams.append('nonce', nonce);
+    
+        const stringPendingEncrypt = `${username}&${password}&${nonce}`;
+    
+        const sign = encryptString(pubkey, stringPendingEncrypt);
+        
+        urlParams.append('sign', sign);
+        
         return fetch(requestURL, {
             method: 'POST',
-            body: url_params,
-            credentials: "same-origin"
+            body: urlParams,
+            credentials: 'same-origin'
         }).then(res => res.json()).catch(console.log);
     }
 
-    form_self.addEventListener('click', e => {
+    form_self.addEventListener('click', async e => {
         if (e.target === submit_btn) {
             e.preventDefault();
             if (submitCheck()) {
-                submit().then(data => {
+                const username = document.getElementById('account');
+                const password = document.getElementById('password');
+                const data_obj = await getNonceAndPubkey();
+                const nonce = data_obj['dataObj'][0];
+                const pubkey = data_obj['dataObj'][1];
+
+                makeRequest(username.value, password.value, nonce, pubkey).then(data => {
                     if (data['flag']) {
+                        const autologin = document.getElementById('autologin');
+                        if (!autologin) {
+                            throw Error(`could not found autologin`);
+                        }
                         // setTimeout(alert(data['errorMsg']));
-                        if (document.getElementById('autologin').checked) {
+                        if (autologin.checked) {
                             fetch('/user/saveLogin', { method: "POST", credentials: "same-origin" });
                         }
-                        location.replace('/');
-                    } else {
+                        window.location.replace('/');
+                    }
+                    else {
                         // TODO: 重做提醒
                         alert(data['errorMsg']);
                     }
@@ -49,13 +88,11 @@ function submitHandler() {
         }
     });
 }
-
-
-
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', () => {
         submitHandler();
     });
-} else {
+}
+else {
     submitHandler();
 }
